cda9c506667b13ce4f9d78ad0dfecb349b652128
wangqianqq <2571524128@qq.com>
2014-12-08 17:03:05 +0800

    �

---
+++ JavaSE/Day07/packageDemo/DemoA.java
@@ -0,0 +1,15 @@
+package jack;
+
+import gem.java.PackDemo;
+
+public class DemoA{
+	String str;
+	PackDemo pd=new PackDemo(); //¼òÐ´
+	//gem.java.PackDemo pd;//È«³Æ
+	
+	public static void main(String[] args){
+		PackDemo pd=new PackDemo();
+		pd.a=123;
+		pd.run();
+	}
+}
---
+++ JavaSE/Day07/packageDemo/PackDemo.java
@@ -0,0 +1,13 @@
+package gem.java;
+
+public class PackDemo{
+	int a;
+	
+	public void run(){
+	System.out.println("默认函数");
+	}
+	/* public static void main(String[] args){
+		System.out.println("hello package");
+	
+	} */
+}

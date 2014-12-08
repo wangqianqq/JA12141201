ea94003aad6cad7a37bcdaf56e23ad2f68db71dc
wangqianqq <2571524128@qq.com>
2014-12-08 15:17:06 +0800

    向上转型向下转型

--- JavaSE/Day07/casting/Sub.java
+++ JavaSE/Day07/casting/Sub.java
@@ -26,8 +26,18 @@ public class Sub extends Base{
 		base.show();
 		
 		//向下转型：子类的引用指向父类的对象。
-		Base ba = new Sub();
+		Sub sub=new Sub();				//sub引用一个子类对象
+		Base b=new Base();				//创建一个父类对象
 		
+		if(b instanceof Sub){			//如果父类的对象实现了子类
+		sub=(Sub)b;					//		将sub的引用指向b	b是父类对象所以需要强制转换
+		}		
+		System.out.println(sub.a);			 //向下转型后可以访问父类的属性
+		sub.show();							//可以访问被子类复写的方法
+		sub.base_show();					//可以访问父类特有的方法
+		sub.sub_show();				//可以访问Sub类特有的方法
+/* 
+		Base ba = new Sub();
 		Sub sub = (Sub)ba;//强制类型转换
 		
 		//访问父类的属性
@@ -37,6 +47,6 @@ public class Sub extends Base{
 		//子类复写父类的方法
 		sub.show();
 		
-		//访问子类的属性
+		//访问子类的属性 */
 	}
 }

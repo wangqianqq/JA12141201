12d6bdd07686bffd580bfd03fd49db2f1b30fc63
wangqianqq <2571524128@qq.com>
2014-12-08 20:40:11 +0800

    多态

--- JavaSE/Day07/casting/N.java
+++ JavaSE/Day07/casting/N.java
@@ -3,6 +3,22 @@
 动物：，猫，狗，猪
 人 男人 女人
 
+1、多态的体现
+父类引用指向子类对象
+2、多态的前提
+（1）必须得类与类之间存在关系，可以是继承关系，也可以是实现关系
+（2）必须得有重写
+3、多态的好处
+大大提高了程序的可扩展性
+4、多态的弊端
+提高的成粗可扩展性，只能用父类的引用访问到父类中的成员
+
+
+
+多态成员变量的特点：
+1、编译期间：参阅的是引用型变量所属的类中是否有调用的方法
+2、运行期间：参阅对象所属的类中是否有调用的方法
+
  */
  class Animal{
 	void sing(){
@@ -14,8 +30,6 @@
  class Dog extends Animal{
 	void sing(){
 		System.out.println("汪汪的唱歌");
-	
-	
 	}
  
  }
@@ -27,27 +41,41 @@
 	}
 	void catchMouse(){
 		System.out.println("捕鼠");
-	
 	}
- 
+}
+
+
+class Pig extends Animal{
+	void sing(){
+		System.out.println("猪之歌");
+	}
  }
  
  public class N{
 	public static void main(String[] args){
+	
 		Cat c=new Cat();
 		/* //c.sing();
 		Cat c1=new Cat();
 		function(c1); */
 		function(c);
-		/* Dog d=new Dog();
+		
+		Dog d=new Dog();
 		function(d);
+		
+		Pig p=new Pig();
+		function(p);
+		
 		/* c1.sing();
 		Cat c2=new Cat();
-		c2.sing();	  */ 
+		c2.sing();	   */
 	}
-	public static void function(Animal c){ //Animal c=new Cat();//子类覆盖父类的方法，通过父类的引用调用方法时实际上调用的是子类方法
+	public static void function(Animal c){ //Animal c=new Cat();
+	//子类覆盖父类的方法，通过父类的引用调用方法时实际上调用的是子类方法
+		
 		c.sing();
 	}
+	
 	/* public static void function(Dog d){
 		d.sing();
 	

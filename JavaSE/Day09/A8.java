7ab3146029a402332e3189d250193976fe333d6a
wangqianqq <2571524128@qq.com>
2014-12-10 20:04:57 +0800

    数组冒泡排序

--- JavaSE/Day09/A8.java
+++ JavaSE/Day09/A8.java
@@ -8,16 +8,11 @@ public class A8{
 					int t=arr[j];
 					arr[j]=arr[j+1];
 					arr[j+1]=t;
-				
 				}
 			}
 		}
 			for(int i=0;i<arr.length;i++){
-				System.out.println(arr[i]);
-			
-		
+				System.out.println(arr[i]);	
 		}
-		
 	}
-
 }

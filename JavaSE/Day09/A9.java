e618b2e180df9a7abbb18c3e3a98f4dd7e5f4299
wangqianqq <2571524128@qq.com>
2014-12-10 20:17:52 +0800

    数组选择排序

--- JavaSE/Day09/A9.java
+++ JavaSE/Day09/A9.java
@@ -1,8 +1,9 @@
+//数组选择排序
 public class A9{
 	public static void main(String[] args){
 		int[] arr={23,45,43,64,34,23,45,89};
-		for(int i=0;i<arr.length;i++){
-			for(int j=i+1;j<arr.length;j++){
+		for(int i=0;i<arr.length;i++){//大的for循环
+			for(int j=i+1;j<arr.length;j++){//小的for循环
 				if(arr[i]>arr[j]){
 					int t=arr[i];
 					arr[i]=arr[j];

130ca55ecdaae24a27e171cc702650d279595082
wangqianqq <2571524128@qq.com>
2014-12-10 19:13:30 +0800

    数组冒泡排序

--- JavaSE/Day09/A8.java
+++ JavaSE/Day09/A8.java
@@ -1,8 +1,8 @@
 public class A8{
 	public static void main(String[] args){
 		int[] arr={34,87,21,45,64,43,78};
-		for(int i=0;i<arr.length-1;i++){
-			for(int j=0;j<arr.length-1;j++){
+		for(int i=0;i<arr.length-1;i++){//大的for循环
+			for(int j=0;j<arr.length-1;j++){//小的for循环
 				if(arr[j]>arr[j+1]){
 					int t=arr[j];
 					arr[j]=arr[j+1];

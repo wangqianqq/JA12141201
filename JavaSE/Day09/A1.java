c324ac8e0f686694c5026672b0cef6b3ce5ec401
wangqianqq <2571524128@qq.com>
2014-12-10 14:35:39 +0800

    数组求最大值

---
+++ JavaSE/Day09/A1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class A1{
+	public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
+		int[] arr=new int[5];
+		for(int i=0;i<arr.length;i++){
+			System.out.println("请输入第"+(i+1)+"个学生的成绩");
+			arr[i]=in.nextInt();
+		}
+		int max=arr[0];
+		for(int i=0;i<arr.length;i++){
+			if(max<arr[i]){
+				max=arr[i];
+			}
+		}
+		System.out.println("最大值为："+max);
+	
+	}
+}

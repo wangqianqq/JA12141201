49774e47e92c4b090160abf69a42a5988dba6105
wangqianqq <2571524128@qq.com>
2014-12-24 19:15:16 +0800

    线程

---
+++ JavaSE/Day24/src/xiancheng/FirstThread.java
@@ -0,0 +1,30 @@
+package xiancheng;
+
+public class FirstThread extends Thread {
+	private int i;
+	
+	//重写run()方法，run()方法的方法体就是线程执行体
+	public void run(){
+		for(;i < 100;i++){
+			//当线程类继承Thread类时，直接使用this即可获取当前线程
+			//Thread对象的getName()返回当前线程的名字
+			//因此可以直接调用getName()方法返回当前线程的名字
+			System.out.println(getName() + ""+i);
+		}
+	}
+	
+	public static void main(String[] args) {
+		for(int i=0;i<100;i++){
+			
+			//调用Thread的currentThread()方法获取当前线程
+			System.out.println(Thread.currentThread().getName()+"" +i);
+			if(i==20){
+				//创建并启动第一个线程
+				new FirstThread().start();
+				//创建并启动第二个线程
+				new FirstThread().start();
+			}
+		}
+	}
+
+}
